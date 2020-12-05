2c248f42433d9c0de59c66b10dd5f3686b115d8d
Your Name <you@example.com>
2020-12-05 02:50:40 +0100

    fix: Stop dropping database when docker starts and do not error
    When creating database if it already exists

--- fe/packages/db-migrations/package-scripts.js
+++ fe/packages/db-migrations/package-scripts.js
@@ -1,3 +1,4 @@
+const child_process = require("child_process");
 const commonScripts = require("../../package-commons-scripts");
 
 const { DATABASE_URL: dbUrl } = process.env;
@@ -25,12 +26,60 @@ module.exports = {
       description: `migrate down`,
     },
     c: {
-      script: `${envs} DATABASE_NAME=postgres db-migrate db:create ${database}`,
+      script: `node -e 'require("./package-scripts").createDb()'`,
+      // script: `${envs} DATABASE_NAME=postgres db-migrate db:create ${database}`,
       description: `Create database`,
     },
     d: {
       script: `${envs} DATABASE_NAME=postgres db-migrate db:drop ${database}`,
       description: `drop database`,
     },
+  },
+  createDb() {
+    let errorCount = 0;
+
+    //kick off process of listing files
+    var child = child_process.spawn(
+      "yarn",
+      ["db-migrate", "db:create", database],
+      {
+        env: {
+          ...process.env,
+          DATABASE_NAME: "postgres",
+          DATABASE_URL: "",
+          DATABASE_HOST: host,
+          DATABASE_PORT: port,
+          DATABASE_USER: user,
+          DATABASE_PASSWORD: password,
+        },
+      }
+    );
+
+    //spit stdout to screen
+    child.stdout.on("data", function (data) {
+      process.stdout.write(data.toString());
+    });
+
+    let okError = false;
+
+    child.stderr.on("data", function (data) {
+      if (errorCount++ === 0) {
+        data = data.toString();
+        const msg = `database "${database}" already exists`;
+
+        if (!data.includes(msg)) {
+          throw new Error(data);
+        }
+
+        process.stdout.write(`\n\n${msg}\n`);
+        okError = true;
+      }
+    });
+
+    child.on("close", function (code) {
+      if (!okError) {
+        console.log("Finished with code " + code);
+      }
+    });
   },
 };
