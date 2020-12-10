c9c2c472fff018152f8e7ed3d0722a9a861de419
Your Name <you@example.com>
2020-12-10 08:19:16 +0100

    chore: use base jest.config.js for pg-promise package

--- fe/packages/pg-promise/jest.config.js
+++ fe/packages/pg-promise/jest.config.js
@@ -1,28 +1,8 @@
-const path = require("path");
+const _config = require("../../_jest.config");
 
-const jestBabelTransform = path.resolve(
-  __dirname,
-  "../../jestBabelTransform.js"
-);
-
-module.exports = {
+const config = {
+  ..._config,
   displayName: "@ta/pg-promise",
-  // projects: ["<rootDir>/../commons/jest.config.js", "<rootDir>/jest.config.js"],
-  clearMocks: true,
-  collectCoverageFrom: ["src/**/*.(ts|js)", "!src/__tests__/**"],
-  coverageDirectory: "coverage",
-  moduleFileExtensions: ["ts", "js"],
-  testEnvironment: "node",
-  testRegex: "src/__tests__/.+?\\.test\\.[tj]s$",
-  transform: {
-    "^.+\\.ts$": jestBabelTransform,
-  },
-  watchPathIgnorePatterns: [
-    "<rootDir>/node_modules*",
-    "<rootDir>/package\\.json",
-    "<rootDir>/package-scripts\\.js",
-    "<rootDir>/jest\\.config\\.js",
-    "<rootDir>/coverage/",
-    "<rootDir>/tsconfig\\.json/",
-  ],
 };
+
+module.exports = config;
