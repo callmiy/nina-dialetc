c35dc86e2bc5826397c47ebfbfc6f5f6a5e587af
Your Name <you@example.com>
2020-12-11 16:54:11 +0100

    test: Shop branch submit empty form - close notification UI

--- fe/packages/svelte-commons/src/__tests__/branch.test.ts
+++ fe/packages/svelte-commons/src/__tests__/branch.test.ts
@@ -3,7 +3,13 @@
  */
 import { render, cleanup } from "@testing-library/svelte";
 import Branch from "../components/branch/branch.svelte";
-import { closeBrandComponentId } from "@ta/cm/src/selectors";
+import {
+  closeBrandComponentId,
+  branchNotificationTextCloseId,
+  branchSubmitId,
+  WARNING_NOTIFICATION_CLASS_NAME,
+  ERROR_NOTIFICATION_CLASS_NAME,
+} from "@ta/cm/src/selectors";
 import { getById } from "@ta/cm/src/__tests__/utils-dom";
 
 let mockId = 0;
@@ -34,5 +40,37 @@ describe("Shop branch", () => {
 
     // Component should not be active
     expect(closeEl.closest(".test-is-active")).toBeNull();
+  });
+
+  it("warns on submission of empty form", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { debug } = render(Branch, {
+      props: {
+        onSubmit: undefined,
+      },
+    });
+
+    // There should be no notification UI
+    expect(getById(branchNotificationTextCloseId)).toBeNull();
+
+    // When form is submitted
+    await getById(branchSubmitId).click();
+
+    // There should be warning notification
+    const notificationEl = getById(branchNotificationTextCloseId);
+    expect(
+      notificationEl.closest(`.${WARNING_NOTIFICATION_CLASS_NAME}`)
+    ).not.toBeNull();
+
+    // There should not be error notification
+    expect(
+      notificationEl.closest(`.${ERROR_NOTIFICATION_CLASS_NAME}`)
+    ).toBeNull();
+
+    // When notification is closed
+    await notificationEl.click();
+
+    // Notification should not be visible
+    expect(getById(branchNotificationTextCloseId)).toBeNull();
   });
 });
