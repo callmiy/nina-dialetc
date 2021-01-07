f3e63ba07ea91d71b8ccaee9c1d180999be19d37
Your Name <you@example.com>
2021-01-07 11:26:03 +0100

    refactor.test: wrap branch test in describe block
    In preparation for edit test

--- fe/packages/svelte-commons/src/__tests__/branch.test.ts
+++ fe/packages/svelte-commons/src/__tests__/branch.test.ts
@@ -32,211 +32,213 @@ describe("Shop branch svelte", () => {
     cleanup();
   });
 
-  it("closes component", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug } = render(Branch, {
-      props: {
-        onSubmit: undefined,
-      },
+  describe("create", () => {
+    it("closes component", async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug } = render(Branch, {
+        props: {
+          onSubmit: undefined,
+        },
+      });
+
+      // Component should be active
+      const closeEl = getById(closeBrandComponentId);
+      expect(closeEl.closest(".test-is-active")).not.toBeNull();
+
+      // When we close component
+      await closeEl.click();
+
+      // Component should not be active
+      expect(closeEl.closest(".test-is-active")).toBeNull();
     });
 
-    // Component should be active
-    const closeEl = getById(closeBrandComponentId);
-    expect(closeEl.closest(".test-is-active")).not.toBeNull();
+    it("warns on submission of empty form", async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug } = render(Branch, {
+        props: {
+          onSubmit: undefined,
+        },
+      });
 
-    // When we close component
-    await closeEl.click();
+      // There should be no notification UI
+      expect(getById(branchNotificationTextCloseId)).toBeNull();
 
-    // Component should not be active
-    expect(closeEl.closest(".test-is-active")).toBeNull();
-  });
+      // When form is submitted
+      await getById(branchSubmitId).click();
 
-  it("warns on submission of empty form", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug } = render(Branch, {
-      props: {
-        onSubmit: undefined,
-      },
+      // There should be warning notification
+      const notificationEl = getById(branchNotificationTextCloseId);
+      expect(
+        notificationEl.closest(`.${WARNING_NOTIFICATION_CLASS_NAME}`)
+      ).not.toBeNull();
+
+      // There should not be error notification
+      expect(
+        notificationEl.closest(`.${ERROR_NOTIFICATION_CLASS_NAME}`)
+      ).toBeNull();
+
+      // When notification is closed
+      await notificationEl.click();
+
+      // Notification should not be visible
+      expect(getById(branchNotificationTextCloseId)).toBeNull();
     });
 
-    // There should be no notification UI
-    expect(getById(branchNotificationTextCloseId)).toBeNull();
+    it("errors on form input errors", async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug } = render(Branch, {
+        props: {
+          onSubmit: undefined,
+        },
+      });
 
-    // When form is submitted
-    await getById(branchSubmitId).click();
+      // When post code field is completed with invalid input
+      const postCodeEl = getById<HTMLInputElement>(branchPostCodeInputId);
+      await fillFieldInput(postCodeEl, "a");
+      expect(postCodeEl.value).toBe("a");
 
-    // There should be warning notification
-    const notificationEl = getById(branchNotificationTextCloseId);
-    expect(
-      notificationEl.closest(`.${WARNING_NOTIFICATION_CLASS_NAME}`)
-    ).not.toBeNull();
+      // When city field is completed with invalid input
+      const cityEl = getById<HTMLInputElement>(branchCityInputId);
+      await fillFieldInput(cityEl, "a");
+      expect(cityEl.value).toBe("a");
 
-    // There should not be error notification
-    expect(
-      notificationEl.closest(`.${ERROR_NOTIFICATION_CLASS_NAME}`)
-    ).toBeNull();
+      // When street field is completed with invalid input
+      const streetEl = getById<HTMLInputElement>(branchStreetInputId);
+      await fillFieldInput(streetEl, "a");
+      expect(streetEl.value).toBe("a");
 
-    // When notification is closed
-    await notificationEl.click();
+      // When alias field is completed
+      const aliasEl = getById<HTMLInputElement>(branchAliasInputId);
+      await fillFieldInput(aliasEl, "a");
+      expect(aliasEl.value).toBe("a");
 
-    // Notification should not be visible
-    expect(getById(branchNotificationTextCloseId)).toBeNull();
-  });
+      // When phone field is completed
+      const phoneEl = getById<HTMLInputElement>(branchPhoneInputId);
+      await fillFieldInput(phoneEl, "a");
+      expect(phoneEl.value).toBe("a");
 
-  it("errors on form input errors", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug } = render(Branch, {
-      props: {
-        onSubmit: undefined,
-      },
+      // There should not be any form fields errors
+      expect(getById(branchPostCodeErrorId)).toBeNull();
+      expect(getById(branchCityErrorId)).toBeNull();
+      expect(getById(branchStreetErrorId)).toBeNull();
+
+      // There should not be an notification
+      expect(getById(branchNotificationTextCloseId)).toBeNull();
+
+      // When form is submitted
+      await getById(branchSubmitId).click();
+
+      // There should be error notification
+      const notificationEl = getById(branchNotificationTextCloseId);
+      expect(
+        notificationEl.closest(`.${ERROR_NOTIFICATION_CLASS_NAME}`)
+      ).not.toBeNull();
+
+      // There should not be warning notification
+      expect(
+        notificationEl.closest(`.${WARNING_NOTIFICATION_CLASS_NAME}`)
+      ).toBeNull();
+
+      // There should be form field errors
+      expect(getById(branchPostCodeErrorId)).not.toBeNull();
+      expect(getById(branchCityErrorId)).not.toBeNull();
+      expect(getById(branchStreetErrorId)).not.toBeNull();
+
+      // When form is reset
+      await getById(branchResetId).click();
+
+      // Form fields should be cleared
+      expect(postCodeEl.value).toBe("");
+      expect(cityEl.value).toBe("");
+      expect(streetEl.value).toBe("");
+      expect(aliasEl.value).toBe("");
+      expect(phoneEl.value).toBe("");
+
+      // There should not be form field errors
+      expect(getById(branchPostCodeErrorId)).toBeNull();
+      expect(getById(branchCityErrorId)).toBeNull();
+      expect(getById(branchStreetErrorId)).toBeNull();
+
+      // There should not be any notification
+      expect(getById(branchNotificationTextCloseId)).toBeNull();
     });
 
-    // When post code field is completed with invalid input
-    const postCodeEl = getById<HTMLInputElement>(branchPostCodeInputId);
-    await fillFieldInput(postCodeEl, "a");
-    expect(postCodeEl.value).toBe("a");
+    it("submits valid form", async () => {
+      mockNewUlid.mockReturnValue("1");
+      const mockOnSubmit = jest.fn();
 
-    // When city field is completed with invalid input
-    const cityEl = getById<HTMLInputElement>(branchCityInputId);
-    await fillFieldInput(cityEl, "a");
-    expect(cityEl.value).toBe("a");
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug } = render(Branch, {
+        props: {
+          onSubmit: mockOnSubmit,
+        },
+      });
 
-    // When street field is completed with invalid input
-    const streetEl = getById<HTMLInputElement>(branchStreetInputId);
-    await fillFieldInput(streetEl, "a");
-    expect(streetEl.value).toBe("a");
+      // When post code field is completed with invalid input
+      const postCodeEl = getById<HTMLInputElement>(branchPostCodeInputId);
+      await fillFieldInput(postCodeEl, branchSubmitVal1.postCode);
 
-    // When alias field is completed
-    const aliasEl = getById<HTMLInputElement>(branchAliasInputId);
-    await fillFieldInput(aliasEl, "a");
-    expect(aliasEl.value).toBe("a");
+      // When city field is completed with invalid input
+      const cityEl = getById<HTMLInputElement>(branchCityInputId);
+      await fillFieldInput(cityEl, branchSubmitVal1.city);
 
-    // When phone field is completed
-    const phoneEl = getById<HTMLInputElement>(branchPhoneInputId);
-    await fillFieldInput(phoneEl, "a");
-    expect(phoneEl.value).toBe("a");
+      // When street field is completed with invalid input
+      const streetEl = getById<HTMLInputElement>(branchStreetInputId);
+      await fillFieldInput(streetEl, branchSubmitVal1.street);
 
-    // There should not be any form fields errors
-    expect(getById(branchPostCodeErrorId)).toBeNull();
-    expect(getById(branchCityErrorId)).toBeNull();
-    expect(getById(branchStreetErrorId)).toBeNull();
+      // When alias field is completed
+      const aliasEl = getById<HTMLInputElement>(branchAliasInputId);
+      await fillFieldInput(aliasEl, branchSubmitVal1.branchAlias);
 
-    // There should not be an notification
-    expect(getById(branchNotificationTextCloseId)).toBeNull();
+      // When phone field is completed
+      const phoneEl = getById<HTMLInputElement>(branchPhoneInputId);
+      await fillFieldInput(phoneEl, branchSubmitVal1.phone);
 
-    // When form is submitted
-    await getById(branchSubmitId).click();
+      expect(mockOnSubmit).not.toBeCalled();
 
-    // There should be error notification
-    const notificationEl = getById(branchNotificationTextCloseId);
-    expect(
-      notificationEl.closest(`.${ERROR_NOTIFICATION_CLASS_NAME}`)
-    ).not.toBeNull();
+      // When form is submitted
+      await getById(branchSubmitId).click();
 
-    // There should not be warning notification
-    expect(
-      notificationEl.closest(`.${WARNING_NOTIFICATION_CLASS_NAME}`)
-    ).toBeNull();
-
-    // There should be form field errors
-    expect(getById(branchPostCodeErrorId)).not.toBeNull();
-    expect(getById(branchCityErrorId)).not.toBeNull();
-    expect(getById(branchStreetErrorId)).not.toBeNull();
-
-    // When form is reset
-    await getById(branchResetId).click();
-
-    // Form fields should be cleared
-    expect(postCodeEl.value).toBe("");
-    expect(cityEl.value).toBe("");
-    expect(streetEl.value).toBe("");
-    expect(aliasEl.value).toBe("");
-    expect(phoneEl.value).toBe("");
-
-    // There should not be form field errors
-    expect(getById(branchPostCodeErrorId)).toBeNull();
-    expect(getById(branchCityErrorId)).toBeNull();
-    expect(getById(branchStreetErrorId)).toBeNull();
-
-    // There should not be any notification
-    expect(getById(branchNotificationTextCloseId)).toBeNull();
-  });
-
-  it("submits valid form", async () => {
-    mockNewUlid.mockReturnValue("1");
-    const mockOnSubmit = jest.fn();
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug } = render(Branch, {
-      props: {
-        onSubmit: mockOnSubmit,
-      },
+      // Form data should be passed to parent
+      expect(mockOnSubmit).toBeCalledWith({
+        ...branchSubmitVal1,
+        id: "1",
+      });
     });
 
-    // When post code field is completed with invalid input
-    const postCodeEl = getById<HTMLInputElement>(branchPostCodeInputId);
-    await fillFieldInput(postCodeEl, branchSubmitVal1.postCode);
+    it("phone and alias may be empty", async () => {
+      const mockOnSubmit = jest.fn();
+      mockNewUlid.mockReturnValue("a");
 
-    // When city field is completed with invalid input
-    const cityEl = getById<HTMLInputElement>(branchCityInputId);
-    await fillFieldInput(cityEl, branchSubmitVal1.city);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug } = render(Branch, {
+        props: {
+          onSubmit: mockOnSubmit,
+        },
+      });
 
-    // When street field is completed with invalid input
-    const streetEl = getById<HTMLInputElement>(branchStreetInputId);
-    await fillFieldInput(streetEl, branchSubmitVal1.street);
+      // When post code field is completed with invalid input
+      const postCodeEl = getById<HTMLInputElement>(branchPostCodeInputId);
+      await fillFieldInput(postCodeEl, branchSubmitVal1.postCode);
 
-    // When alias field is completed
-    const aliasEl = getById<HTMLInputElement>(branchAliasInputId);
-    await fillFieldInput(aliasEl, branchSubmitVal1.branchAlias);
+      // When city field is completed with invalid input
+      const cityEl = getById<HTMLInputElement>(branchCityInputId);
+      await fillFieldInput(cityEl, branchSubmitVal1.city);
 
-    // When phone field is completed
-    const phoneEl = getById<HTMLInputElement>(branchPhoneInputId);
-    await fillFieldInput(phoneEl, branchSubmitVal1.phone);
+      // When street field is completed with invalid input
+      const streetEl = getById<HTMLInputElement>(branchStreetInputId);
+      await fillFieldInput(streetEl, branchSubmitVal1.street);
 
-    expect(mockOnSubmit).not.toBeCalled();
+      // When form is submitted
+      await getById(branchSubmitId).click();
 
-    // When form is submitted
-    await getById(branchSubmitId).click();
-
-    // Form data should be passed to parent
-    expect(mockOnSubmit).toBeCalledWith({
-      ...branchSubmitVal1,
-      id: "1",
-    });
-  });
-
-  it("phone and alias may be empty", async () => {
-    const mockOnSubmit = jest.fn();
-    mockNewUlid.mockReturnValue("a");
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug } = render(Branch, {
-      props: {
-        onSubmit: mockOnSubmit,
-      },
-    });
-
-    // When post code field is completed with invalid input
-    const postCodeEl = getById<HTMLInputElement>(branchPostCodeInputId);
-    await fillFieldInput(postCodeEl, branchSubmitVal1.postCode);
-
-    // When city field is completed with invalid input
-    const cityEl = getById<HTMLInputElement>(branchCityInputId);
-    await fillFieldInput(cityEl, branchSubmitVal1.city);
-
-    // When street field is completed with invalid input
-    const streetEl = getById<HTMLInputElement>(branchStreetInputId);
-    await fillFieldInput(streetEl, branchSubmitVal1.street);
-
-    // When form is submitted
-    await getById(branchSubmitId).click();
-
-    // Form data should be passed to parent
-    expect(mockOnSubmit).toBeCalledWith({
-      ...branchSubmitVal1,
-      id: "a",
-      phone: null,
-      branchAlias: null,
+      // Form data should be passed to parent
+      expect(mockOnSubmit).toBeCalledWith({
+        ...branchSubmitVal1,
+        id: "a",
+        phone: null,
+        branchAlias: null,
+      });
     });
   });
 });
