05d7a60d6c2d869560365144807e99c9317f0262
Your Name <you@example.com>
2021-01-11 03:26:56 +0100

    msw - move handlers into own file and delete unneeded handler code

--- fe/packages/commons/src/__tests__/msw-handlers.ts
+++ fe/packages/commons/src/__tests__/msw-handlers.ts
@@ -2,18 +2,43 @@ import { graphql } from "msw";
 import {
   ListBranches,
   ListBranchesVariables,
-  ListBranches_listBranches,
+  ListBrands,
+  ListBrandsVariables,
 } from "../gql/ops-types";
+import { mockBranchValue1, mockBrandValue1 } from "./mock-data";
 
-export function makeListBranchesHandler(data: ListBranches_listBranches) {
-  return graphql.query<ListBranches, ListBranchesVariables>(
+export const handlers = [
+  graphql.query<ListBranches, ListBranchesVariables>(
     "ListBranches",
     (req, res, ctx) => {
       return res(
         ctx.data({
-          listBranches: data,
+          listBranches: {
+            edges: [
+              {
+                node: mockBranchValue1,
+              },
+            ],
+          },
         })
       );
     }
-  );
-}
+  ),
+
+  graphql.query<ListBrands, ListBrandsVariables>(
+    "ListBrands",
+    (req, res, ctx) => {
+      return res(
+        ctx.data({
+          listBrands: {
+            edges: [
+              {
+                node: mockBrandValue1,
+              },
+            ],
+          },
+        })
+      );
+    }
+  ),
+];
--- fe/packages/commons/src/__tests__/msw-server.ts
+++ fe/packages/commons/src/__tests__/msw-server.ts
@@ -1,48 +1,5 @@
 import { setupServer } from "msw/node";
-import { graphql } from "msw";
-import {
-  ListBranches,
-  ListBranchesVariables,
-  ListBrands,
-  ListBrandsVariables,
-} from "../gql/ops-types";
-import { mockBranchValue1, mockBrandValue1 } from "./mock-data";
-
-const handlers = [
-  graphql.query<ListBranches, ListBranchesVariables>(
-    "ListBranches",
-    (req, res, ctx) => {
-      return res(
-        ctx.data({
-          listBranches: {
-            edges: [
-              {
-                node: mockBranchValue1,
-              },
-            ],
-          },
-        })
-      );
-    }
-  ),
-
-  graphql.query<ListBrands, ListBrandsVariables>(
-    "ListBrands",
-    (req, res, ctx) => {
-      return res(
-        ctx.data({
-          listBrands: {
-            edges: [
-              {
-                node: mockBrandValue1,
-              },
-            ],
-          },
-        })
-      );
-    }
-  ),
-];
+import { handlers } from "./msw-handlers";
 
 // Setup requests interception using the given handlers.
 export const mswServer = setupServer(...handlers);
