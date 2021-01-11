584b3ba8efeca988b120d90c805d5c4fb0452e60
Your Name <you@example.com>
2021-01-11 02:11:42 +0100

    brand test: wrap in describe 'create' block

--- fe/packages/svelte-commons/src/__tests__/brand.test.ts
+++ fe/packages/svelte-commons/src/__tests__/brand.test.ts
@@ -37,7 +37,6 @@ import {
   cleanup,
   render,
   waitFor,
-  waitForElementToBeRemoved,
 } from "@testing-library/svelte";
 import Brand from "../components/brand/brand.svelte";
 import { resetCountriesCurrenciesStore } from "../stores/get-countries-and-currencies.store";
@@ -94,250 +93,256 @@ describe("Brand svelte", () => {
     },
   ] as GetCountriesCurrencies;
 
-  it(`reset form /
+  describe("create", () => {
+    it(`reset form /
     submit empty form`, async () => {
-    mockGetCountriesCurrencies.mockResolvedValue(countriesCurrencies);
+      mockGetCountriesCurrencies.mockResolvedValue(countriesCurrencies);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug, container } = render(Brand, {
-      props: {
-        isActive: true,
-        onSubmit: undefined,
-      },
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug, container } = render(Brand, {
+        props: {
+          isActive: true,
+          onSubmit: undefined,
+        },
+      });
+
+      // Wait for countries and currencies data to resolve
+      await waitFor(() => true); // not enough, see below!!
+
+      // When form is completed
+      const nameInputEl = getById<HTMLInputElement>(brandNameInputDomId);
+      await fillFieldInput(nameInputEl, ""); // just to let fetched data resolve
+      await fillFieldInput(nameInputEl, "a");
+      expect(nameInputEl.value).toBe("a");
+
+      const countryInputEl = getById<HTMLInputElement>(brandCountryInputDomId);
+      await fillFieldChange(countryInputEl, "co1");
+      expect(countryInputEl.value).toBe("co1");
+
+      const currencyInputEl = getById<HTMLInputElement>(
+        brandCurrencyInputDomId
+      );
+      await fillFieldChange(currencyInputEl, "cur1");
+      expect(currencyInputEl.value).toBe("cur1");
+
+      const phoneInputEl = getById<HTMLInputElement>(brandPhoneInputDomId);
+      await fillFieldInput(phoneInputEl, "a");
+      expect(phoneInputEl.value).toBe("a");
+
+      // When reset button is clicked
+      const resetEl = getById(resetFormBtnId);
+      await resetEl.click();
+
+      // Form should be empty
+      expect(nameInputEl.value).toBe("");
+      expect(countryInputEl.value).toBe("");
+      expect(currencyInputEl.value).toBe("");
+      expect(phoneInputEl.value).toBe("");
+
+      // Warning notification that form empty should not visible
+      expect(getById(brandNotificationTextCloseId)).toBeNull();
+
+      // When empty form is submitted
+      const submitEl = getById(brandSubmitId);
+      await submitEl.click();
+
+      // Warning notification is visible
+      const closeNotificationEl = getById(brandNotificationTextCloseId);
+      const closeParentEl = closeNotificationEl.closest(
+        `.${WARNING_NOTIFICATION_CLASS_NAME}`
+      ) as HTMLElement;
+
+      expect(closeParentEl.classList).not.toContain(
+        ERROR_NOTIFICATION_CLASS_NAME
+      );
+
+      // When reset button is clicked
+      await resetEl.click();
+
+      // Warning notification should not visible
+      expect(getById(brandNotificationTextCloseId)).toBeNull();
+
+      // The form should not be disabled
+      container.querySelectorAll("fieldset").forEach((f) => {
+        expect((f as HTMLFieldSetElement).disabled).toBe(false);
+      });
     });
 
-    // Wait for countries and currencies data to resolve
-    await waitFor(() => true); // not enough, see below!!
+    it(`currencies and countries loading`, async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug, container } = render(Brand, {
+        props: {
+          onSubmit: undefined,
+        },
+      });
 
-    // When form is completed
-    const nameInputEl = getById<HTMLInputElement>(brandNameInputDomId);
-    await fillFieldInput(nameInputEl, ""); // just to let fetched data resolve
-    await fillFieldInput(nameInputEl, "a");
-    expect(nameInputEl.value).toBe("a");
+      // Wait for countries and currencies data to resolve
+      await waitFor(() => true);
 
-    const countryInputEl = getById<HTMLInputElement>(brandCountryInputDomId);
-    await fillFieldChange(countryInputEl, "co1");
-    expect(countryInputEl.value).toBe("co1");
+      // Country field should indicate loading
+      const countryLoadingEl = getById(brandCountryMsgDomId);
+      expect(countryLoadingEl.textContent).toBe(COUNTRIES_LOADING_MSG);
 
-    const currencyInputEl = getById<HTMLInputElement>(brandCurrencyInputDomId);
-    await fillFieldChange(currencyInputEl, "cur1");
-    expect(currencyInputEl.value).toBe("cur1");
+      // Currency field error should indicate loading
+      const currencyLoadingEl = getById(brandCurrencyMsgDomId);
+      expect(currencyLoadingEl.textContent).toBe(CURRENCIES_LOADING_MSG);
 
-    const phoneInputEl = getById<HTMLInputElement>(brandPhoneInputDomId);
-    await fillFieldInput(phoneInputEl, "a");
-    expect(phoneInputEl.value).toBe("a");
-
-    // When reset button is clicked
-    const resetEl = getById(resetFormBtnId);
-    await resetEl.click();
-
-    // Form should be empty
-    expect(nameInputEl.value).toBe("");
-    expect(countryInputEl.value).toBe("");
-    expect(currencyInputEl.value).toBe("");
-    expect(phoneInputEl.value).toBe("");
-
-    // Warning notification that form empty should not visible
-    expect(getById(brandNotificationTextCloseId)).toBeNull();
-
-    // When empty form is submitted
-    const submitEl = getById(brandSubmitId);
-    await submitEl.click();
-
-    // Warning notification is visible
-    const closeNotificationEl = getById(brandNotificationTextCloseId);
-    const closeParentEl = closeNotificationEl.closest(
-      `.${WARNING_NOTIFICATION_CLASS_NAME}`
-    ) as HTMLElement;
-
-    expect(closeParentEl.classList).not.toContain(
-      ERROR_NOTIFICATION_CLASS_NAME
-    );
-
-    // When reset button is clicked
-    await resetEl.click();
-
-    // Warning notification should not visible
-    expect(getById(brandNotificationTextCloseId)).toBeNull();
-
-    // The form should not be disabled
-    container.querySelectorAll("fieldset").forEach((f) => {
-      expect((f as HTMLFieldSetElement).disabled).toBe(false);
-    });
-  });
-
-  it(`currencies and countries loading`, async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug, container } = render(Brand, {
-      props: {
-        onSubmit: undefined,
-      },
+      // The form should be disabled
+      container.querySelectorAll("fieldset").forEach((f) => {
+        expect((f as HTMLFieldSetElement).disabled).toBe(true);
+      });
     });
 
-    // Wait for countries and currencies data to resolve
-    await waitFor(() => true);
-
-    // Country field should indicate loading
-    const countryLoadingEl = getById(brandCountryMsgDomId);
-    expect(countryLoadingEl.textContent).toBe(COUNTRIES_LOADING_MSG);
-
-    // Currency field error should indicate loading
-    const currencyLoadingEl = getById(brandCurrencyMsgDomId);
-    expect(currencyLoadingEl.textContent).toBe(CURRENCIES_LOADING_MSG);
-
-    // The form should be disabled
-    container.querySelectorAll("fieldset").forEach((f) => {
-      expect((f as HTMLFieldSetElement).disabled).toBe(true);
-    });
-  });
-
-  it(`brand name too short /
+    it(`brand name too short /
       currency and country empty /
       close error notification`, async () => {
-    mockGetCountriesCurrencies.mockResolvedValue(countriesCurrencies);
+      mockGetCountriesCurrencies.mockResolvedValue(countriesCurrencies);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug } = render(Brand, {
-      props: {
-        onSubmit: undefined,
-      },
-    });
-
-    // Wait for countries and currencies data to resolve
-    await waitFor(() => true);
-
-    // Component should always be active
-    const componentEl = getById(brandDomId);
-    expect(componentEl.classList).toContain(IS_ACTIVE_CLASS_NAME);
-
-    // When we fill shop brand name incorrectly (too short)
-    const nameInputEl = getById<HTMLInputElement>(brandNameInputDomId);
-    await fillFieldInput(nameInputEl, ""); // just to let fetched data resolve
-    await fillFieldInput(nameInputEl, "a");
-
-    // Brand name field error should not be visible
-    expect(getById(brandNameErrorDomId)).toBeNull();
-
-    // Country field error should not be visible
-    expect(getById(brandCountryMsgDomId)).toBeNull();
-
-    // Currency field error should not be visible
-    expect(getById(brandCurrencyMsgDomId)).toBeNull();
-
-    // General form error should not be visible
-    expect(getById(brandNotificationTextCloseId)).toBeNull();
-
-    // When we submit form
-    const submitEl = getById(brandSubmitId);
-    await submitEl.click();
-
-    // Brand name field error should be visible (too short)
-    expect(getById(brandNameErrorDomId)).not.toBeNull();
-
-    // Country field error should be visible (empty)
-    expect(getById(brandCountryMsgDomId)).not.toBeNull();
-
-    // Currency field error should be visible (empty)
-    expect(getById(brandCurrencyMsgDomId)).not.toBeNull();
-
-    // General form error should be visible
-    const closeNotificationEl = getById(brandNotificationTextCloseId);
-    const closeParentEl = closeNotificationEl.closest(
-      `.${ERROR_NOTIFICATION_CLASS_NAME}`
-    ) as HTMLElement;
-
-    expect(closeParentEl.classList).not.toContain(
-      WARNING_NOTIFICATION_CLASS_NAME
-    );
-
-    // When notification is closed
-    await closeNotificationEl.click();
-
-    // General form error should not be visible
-    expect(getById(brandNotificationTextCloseId)).toBeNull();
-  });
-
-  it(`submits correct form data /
-      nullable fields returned as null when cleared`, async () => {
-    mockGetCountriesCurrencies.mockResolvedValue(countriesCurrencies);
-    mockNewUlid.mockReturnValue(1);
-    const mockOnSubmit = jest.fn();
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { debug } = render(Brand, {
-      props: {
-        onSubmit: mockOnSubmit as any,
-      },
-    });
-
-    // Wait for countries and currencies data to resolve
-    const countryInputEl = getById<HTMLInputElement>(brandCountryInputDomId);
-
-    const germanyEl = await waitForCount(async () => {
-      return await waitFor(() => {
-        const x = countryInputEl.querySelector(
-          `option[value="${germanyCountry.id}"]`
-        );
-
-        return x;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug } = render(Brand, {
+        props: {
+          onSubmit: undefined,
+        },
       });
-    }, 10);
 
-    expect(germanyEl).not.toBeNull();
-    await fillFieldChange(countryInputEl, germanyCountry.id);
+      // Wait for countries and currencies data to resolve
+      await waitFor(() => true);
 
-    // When form is completed with valid data
-    const nameInputEl = getById<HTMLInputElement>(brandNameInputDomId);
-    await fillFieldInput(nameInputEl, validName);
+      // Component should always be active
+      const componentEl = getById(brandDomId);
+      expect(componentEl.classList).toContain(IS_ACTIVE_CLASS_NAME);
 
-    const currencyInputEl = getById<HTMLInputElement>(brandCurrencyInputDomId);
-    await fillFieldChange(currencyInputEl, eurCcy.id);
+      // When we fill shop brand name incorrectly (too short)
+      const nameInputEl = getById<HTMLInputElement>(brandNameInputDomId);
+      await fillFieldInput(nameInputEl, ""); // just to let fetched data resolve
+      await fillFieldInput(nameInputEl, "a");
 
-    const phoneInputEl = getById<HTMLInputElement>(brandPhoneInputDomId);
-    await fillFieldInput(phoneInputEl, phoneNum);
+      // Brand name field error should not be visible
+      expect(getById(brandNameErrorDomId)).toBeNull();
 
-    // submit should not have been called
-    expect(mockOnSubmit).not.toBeCalled();
+      // Country field error should not be visible
+      expect(getById(brandCountryMsgDomId)).toBeNull();
 
-    // When we submit form
-    const submitEl = getById(brandSubmitId);
-    await submitEl.click();
+      // Currency field error should not be visible
+      expect(getById(brandCurrencyMsgDomId)).toBeNull();
 
-    // Brand name field error should not be visible
-    expect(getById(brandNameErrorDomId)).toBeNull();
+      // General form error should not be visible
+      expect(getById(brandNotificationTextCloseId)).toBeNull();
 
-    // Country field error should not be visible
-    expect(getById(brandCountryMsgDomId)).toBeNull();
+      // When we submit form
+      const submitEl = getById(brandSubmitId);
+      await submitEl.click();
 
-    // Currency field error should not be visible
-    expect(getById(brandCurrencyMsgDomId)).toBeNull();
+      // Brand name field error should be visible (too short)
+      expect(getById(brandNameErrorDomId)).not.toBeNull();
 
-    // General form error should not be visible
-    expect(getById(brandNotificationTextCloseId)).toBeNull();
+      // Country field error should be visible (empty)
+      expect(getById(brandCountryMsgDomId)).not.toBeNull();
 
-    // form data should be passed to parent
-    expect(mockOnSubmit).toBeCalledWith({
-      id: 1,
-      name: validName,
-      country: germanyCountry,
-      currency: eurCcy,
-      phone: phoneNum,
+      // Currency field error should be visible (empty)
+      expect(getById(brandCurrencyMsgDomId)).not.toBeNull();
+
+      // General form error should be visible
+      const closeNotificationEl = getById(brandNotificationTextCloseId);
+      const closeParentEl = closeNotificationEl.closest(
+        `.${ERROR_NOTIFICATION_CLASS_NAME}`
+      ) as HTMLElement;
+
+      expect(closeParentEl.classList).not.toContain(
+        WARNING_NOTIFICATION_CLASS_NAME
+      );
+
+      // When notification is closed
+      await closeNotificationEl.click();
+
+      // General form error should not be visible
+      expect(getById(brandNotificationTextCloseId)).toBeNull();
     });
 
-    // when phone number field is cleared
-    await fillFieldInput(phoneInputEl, "");
+    it(`submits correct form data /
+      nullable fields returned as null when cleared`, async () => {
+      mockGetCountriesCurrencies.mockResolvedValue(countriesCurrencies);
+      mockNewUlid.mockReturnValue(1);
+      const mockOnSubmit = jest.fn();
 
-    // when form is submitted
-    mockOnSubmit.mockReset();
-    await submitEl.click();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { debug } = render(Brand, {
+        props: {
+          onSubmit: mockOnSubmit as any,
+        },
+      });
 
-    // form data should be passed to parent with nullable fields set to null
-    expect(mockOnSubmit).toBeCalledWith({
-      id: 1,
-      name: validName,
-      country: germanyCountry,
-      currency: eurCcy,
-      phone: null,
+      // Wait for countries and currencies data to resolve
+      const countryInputEl = getById<HTMLInputElement>(brandCountryInputDomId);
+
+      const germanyEl = await waitForCount(async () => {
+        return await waitFor(() => {
+          const x = countryInputEl.querySelector(
+            `option[value="${germanyCountry.id}"]`
+          );
+
+          return x;
+        });
+      }, 10);
+
+      expect(germanyEl).not.toBeNull();
+      await fillFieldChange(countryInputEl, germanyCountry.id);
+
+      // When form is completed with valid data
+      const nameInputEl = getById<HTMLInputElement>(brandNameInputDomId);
+      await fillFieldInput(nameInputEl, validName);
+
+      const currencyInputEl = getById<HTMLInputElement>(
+        brandCurrencyInputDomId
+      );
+      await fillFieldChange(currencyInputEl, eurCcy.id);
+
+      const phoneInputEl = getById<HTMLInputElement>(brandPhoneInputDomId);
+      await fillFieldInput(phoneInputEl, phoneNum);
+
+      // submit should not have been called
+      expect(mockOnSubmit).not.toBeCalled();
+
+      // When we submit form
+      const submitEl = getById(brandSubmitId);
+      await submitEl.click();
+
+      // Brand name field error should not be visible
+      expect(getById(brandNameErrorDomId)).toBeNull();
+
+      // Country field error should not be visible
+      expect(getById(brandCountryMsgDomId)).toBeNull();
+
+      // Currency field error should not be visible
+      expect(getById(brandCurrencyMsgDomId)).toBeNull();
+
+      // General form error should not be visible
+      expect(getById(brandNotificationTextCloseId)).toBeNull();
+
+      // form data should be passed to parent
+      expect(mockOnSubmit).toBeCalledWith({
+        id: 1,
+        name: validName,
+        country: germanyCountry,
+        currency: eurCcy,
+        phone: phoneNum,
+      });
+
+      // when phone number field is cleared
+      await fillFieldInput(phoneInputEl, "");
+
+      // when form is submitted
+      mockOnSubmit.mockReset();
+      await submitEl.click();
+
+      // form data should be passed to parent with nullable fields set to null
+      expect(mockOnSubmit).toBeCalledWith({
+        id: 1,
+        name: validName,
+        country: germanyCountry,
+        currency: eurCcy,
+        phone: null,
+      });
     });
   });
 });
