57c30c165c06defef733e0463dc332c5af6e7769
Kanmii <maneptha@gmail.com>
2021-01-24 00:05:29 +0100

    routes/index; extract shoppingRoute to be re-used in home and /shopping

--- fe/packages/svelte/src/routes/index.ts
+++ fe/packages/svelte/src/routes/index.ts
@@ -1,14 +1,18 @@
 import { wrap } from "svelte-spa-router/wrap";
 import Loading from "@ta/sc/src/components/loading.svelte";
 
+const shoppingRoute = wrap({
+  asyncComponent: () => import("./shopping.route.svelte"),
+  loadingComponent: Loading,
+});
+
 export const routes = {
+  "/shopping/": shoppingRoute,
+
   "/signup": wrap({
     asyncComponent: () => import("./signup.route.svelte"),
     loadingComponent: Loading,
   }),
 
-  "/shopping": wrap({
-    asyncComponent: () => import("./shopping.route.svelte"),
-    loadingComponent: Loading,
-  }),
+  "/": shoppingRoute,
 };
